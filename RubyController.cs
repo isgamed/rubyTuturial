9d1b56ec2eed24d0beca200d8a2050231968f54c
isgamed <66990760+isgamed@users.noreply.github.com>
2020-06-20 16:19:47 +0300

    Add files via upload

---
+++ RubyController.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
+
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+    bool isInvincible;
+    float invincibleTimer;
+
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
+    Rigidbody2D rigidbody2d;
+
+    public GameObject projectilePrefab;
+    public ParticleSystem damageEffect;
+    public ParticleSystem CollectibleEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
+
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
+
+    }
+        public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            
+            if (isInvincible)
+                return;
+
+            animator.SetTrigger("Hit");
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+            ParticleSystem damageParticle = Instantiate(damageEffect, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        }
+        if (amount > 0)
+        {
+            ParticleSystem collectParticle = Instantiate(CollectibleEffect, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+        }
+            currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+            Debug.Log(currentHealth + "/" + maxHealth);
+            
+    }
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
+    }
+
+}
